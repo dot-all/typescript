a0919cf557dd2cba5b62ce10ff4777bd506ff769
dot-all <al.quirozsilva@gmail.com>
2024-06-25 23:13:23 -0400

    feat:: add tests to binaryBitwise function

--- bitwise/src/binary.test.ts
+++ bitwise/src/binary.test.ts
@@ -1,4 +1,4 @@
-import { binaryBase2 } from ".";
+import { binaryBase2, binaryBitwise } from ".";
 
 describe("Testing binaryBase2 function", () => {
   it("should convert 10 to binary [1, 0, 1, 0]", () => {
@@ -9,5 +9,17 @@ describe("Testing binaryBase2 function", () => {
   })
   it("should convert 1234321 to binary [1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1]", () => {
     expect(binaryBase2(1234321)).toEqual([1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1]);
+  })
+})
+
+describe("Testing binaryBitwise function", () => {
+  it("should convert 27 to binary [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1]", () => {
+    expect(binaryBitwise(27)).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1]);
+  })
+  it("should convert 3 to binary [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]", () => {
+    expect(binaryBitwise(3)).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1]);
+  })
+  it("should convert 12345678 to binary [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0]", () => {
+    expect(binaryBitwise(12345678)).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0]);
   })
 })
