251c7e8c0a2003aad4ca7b07ad6fc7997bc71716
dot-all <al.quirozsilva@gmail.com>
2024-06-25 21:06:19 -0400

    feat: add decimal to binary conversion with bitwise operations

--- bitwise/src/index.ts
+++ bitwise/src/index.ts
@@ -1,4 +1,4 @@
-
+// Base 2
 const binaryBase2 = (number: number): Array<number> => {
   let bits = [];
   
@@ -14,4 +14,22 @@ let number: number = 1000;
 
 const numberInBinary = binaryBase2(number);
 
-console.log(`The number ${number} in binary is: ${numberInBinary}`);
+console.log(`The number ${number} in binary base 2 is: ${numberInBinary}`);
+
+
+// Bitwise
+
+const binaryBitwise = (number: number): Array<number> => {
+  let bits = [];
+  
+  for (let b = 31; b >= 0 ; b--) {
+    let bit = (number >> b) & 1;
+    bits.push(bit);
+  }
+  return bits;
+}
+
+let numberToBit: number = 1000;
+const numberInBinaryBitwise = binaryBitwise(numberToBit);
+
+console.log(`The number ${number} in binary bitwise is: ${numberInBinaryBitwise}`);
