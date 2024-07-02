442c13cbd8324e99de409f401e66abdfcda20f08
dot-all <al.quirozsilva@gmail.com>
2024-07-02 16:59:58 -0400

    chore: add example of use of the selectionSort function

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -37,3 +37,6 @@ function selectionSort(arr: number[]): number[] {
   }
   return arr;
 }
+
+const selectionSortedArray = selectionSort([64, 25, 12, 22, 11]);
+console.log("Selection Sorted Array:", selectionSortedArray);
